0fce37dd17988ab378be1bcc7cb8009d041c3342
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-02-28 12:09:20 -0600

    create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+create table animals(id bigserial not null primary key, name varchar(20), date_of_birth date, escape_attempts integer, neutered boolean, weight_Kg decimal);
+alter table animals add column species varchar(20);
