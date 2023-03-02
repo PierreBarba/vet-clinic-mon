c6de2a41c66c317ffe59b69201f37a402c927e6f
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-01 23:15:27 -0600

    NEW TABLES

--- schema.sql
+++ schema.sql
@@ -1,4 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-create table animals(id bigserial not null primary key, name varchar(20), date_of_birth date, escape_attempts integer, neutered boolean, weight_Kg decimal);
-alter table animals add column species varchar(30);
+CREATE TABLE animals(id bigserial not null primary key, name varchar(20), date_of_birth date, escape_attempts integer, neutered boolean, weight_Kg decimal);
+ALTER TABLE animals add column species varchar(30);
+
+CREATE TABLE owners (id int generated always as identity, full_name varchar(50), age integer, primary key(id));
+CREATE TABLE species(id int generated always as identity, name varchar(50), primary key(id));
+
+ALTER TABLE animals DROP column species;
+ALTER TABLE animals ADD column species_id integer;
+ALTER TABLE animals ADD constraint fk_constraint foreign key(species_id) references species(id);
+ALTER TABLE animals ADD column owner_id integer;
+ALTER TABLE animals ADD constraint fk_constraint2 foreign key(owner_id) references owners(id);
