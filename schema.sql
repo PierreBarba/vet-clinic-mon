4ae1b2f9e61c968d031de1795b79695b7a337d69
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-03 10:04:27 -0600

    ADD NEW SCHEMA

--- schema.sql
+++ schema.sql
@@ -11,3 +11,7 @@ ALTER TABLE animals ADD column species_id integer;
 ALTER TABLE animals ADD constraint fk_constraint foreign key(species_id) references species(id);
 ALTER TABLE animals ADD column owner_id integer;
 ALTER TABLE animals ADD constraint fk_constraint2 foreign key(owner_id) references owners(id);
+
+CREATE TABLE vets (id int generated always as identity, name varchar(50), age integer,date_of_graduation date, primary key(id));
+CREATE TABLE visits (animals_id int references animals(id),vets_id int references vets(id),date_of_visit date);
+CREATE TABLE specializations (species_id int references species(id),vets_id int references vets(id), PRIMARY KEY(species_id,vets_id));
