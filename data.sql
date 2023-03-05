f046d189b3fc4856bdf3e6c13af7feadd3eb5d01
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-05 14:05:08 -0600

    ADD NEW DATA

--- data.sql
+++ data.sql
@@ -59,3 +59,7 @@ INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, '2020-02-2
 INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, '2020-08-03'),
 INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (10, 3, '2020-05-24'),
 INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (10, 1, '2021-01-11');
+
+
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+INSERT INTO owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
