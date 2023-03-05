2366464713554168ae79e861fba84bbe30411b9f
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-05 14:04:53 -0600

    MODIFY SCHEMA

--- schema.sql
+++ schema.sql
@@ -19,4 +19,3 @@ CREATE TABLE specializations (species_id int references species(id),vets_id int 
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 INSERT INTO owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
