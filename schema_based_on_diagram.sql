0486e52b4f15b9af24a8f8caee98b41f623dd2f5
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-06 13:16:19 -0600

    create table treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,5 @@ CREATE DATABASE clinic;
 CREATE TABLE patients(id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), date_of_birth DATE);
 
 CREATE TABLE medical_histories(id SERIAL NOT NULL PRIMARY KEY,admitted_at TIMESTAMP,patient_id INT REFERENCES patients(id),status VARCHAR(100));
+
+CREATE TABLE treatments(id SERIAL NOT NULL PRIMARY KEY,type vARCHAR(100),name VARCHAR(100));
