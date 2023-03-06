8625949ca02bdcc7870ae49a7015ce06cb0f9eb8
Wali-Muhammad <luckywali0986@gmail.com>
2023-03-07 00:16:54 +0500

    Create histories tratment table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,5 @@ CREATE TABLE patients(id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), date_of
 CREATE TABLE medical_histories(id SERIAL NOT NULL PRIMARY KEY,admitted_at TIMESTAMP,patient_id INT REFERENCES patients(id),status VARCHAR(100));
 
 CREATE TABLE treatments(id SERIAL NOT NULL PRIMARY KEY,type vARCHAR(100),name VARCHAR(100));
+
+CREATE TABLE medical_histories_treatments (medical_histories_id INT REFERENCES medical_histories(id),treatments INT REFERENCES treatments(id));
