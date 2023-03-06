2852cc2eeb57a65406633ed886c9e3653e6d4b58
Wali-Muhammad <luckywali0986@gmail.com>
2023-03-07 00:15:09 +0500

    Create Table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE clinic;
 
 CREATE TABLE patients(id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), date_of_birth DATE);
+
+CREATE TABLE medical_histories(id SERIAL NOT NULL PRIMARY KEY,admitted_at TIMESTAMP,patient_id INT REFERENCES patients(id),status VARCHAR(100));
