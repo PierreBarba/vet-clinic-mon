77772a32bfbada5d5955fe3dd46a16f79fdfd65d
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-06 13:17:49 -0600

    create table invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,7 @@ CREATE TABLE medical_histories(id SERIAL NOT NULL PRIMARY KEY,admitted_at TIMEST
 CREATE TABLE treatments(id SERIAL NOT NULL PRIMARY KEY,type vARCHAR(100),name VARCHAR(100));
 
 CREATE TABLE medical_histories_treatments (medical_histories_id INT REFERENCES medical_histories(id),treatments INT REFERENCES treatments(id));
+
+CREATE TABLE invoices ( id SERIAL NOT NULL PRIMARY KEY,total_amount DECIMAL,generated_at TIMESTAMP,payed_at TIMESTAMP,
+    medical_history_id INT NOT NULL,UNIQUE(medical_history_id),
+    FOREIGN KEY(medical_history_id) references medical_histories(id));
